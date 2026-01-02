35d7775427abeb83e88e6e2da5e617425d8fda7d
U-S-MOHANLAL <smohanlal281@gmail.com>
2026-01-02 19:43:54 +0530

    Search By Name API Added

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
-
+import yfinance
+import pandas as pd
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {
-        "message": "Hello, World!"
-    }
+@app.get("/search/")
+async def root(name: str):
+    tickersdf = pd.DataFrame(yfinance.Lookup(name).all)
+    if tickersdf.to_dict() == {}:
+        return {"tickers": []}
+    else:
+        tickersdict = tickersdf.to_dict()['shortName']
+        tickerlist = list(tickersdict.values())
+        return {"tickers": cleanTickerList(tickerlist)}
+
+def cleanTickerList(tickerlist):
+    return [key for key in tickerlist if not pd.isna(key)]
